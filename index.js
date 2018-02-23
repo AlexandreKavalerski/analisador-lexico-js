d63291afb1e85a182c67be0aef4f3abade286856
Alexandre Henrique Kavalerski Teixeira <kavalerskialexandre@gmail.com>
2018-02-23 17:49:53 -0300

    Corrigindo identação

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const palavras_reservadas = ['if', 'var', 'console.log', 'else', 'for']
-const operadores = ['+','-','/','*','<','>','=','%','!']
-const terminadores = [';','(',')','[',']','{','}','"'] 
+const operadores = ['+', '-', '/', '*', '<', '>', '=', '%', '!']
+const terminadores = [';', '(', ')', '[', ']', '{', '}', '"']
 txt_entrada = `
     var i = 600;
     if ( i % 2 == 0 ) {
@@ -15,81 +15,81 @@ txt_entrada = `
 `
 
 var token_atual = '';                   // Armazena a sequencia de caracteres que forma um token
-var pr_encontrados = [];                
-var identificadores_encontrados = [];   
-var operadores_encontrados = [];        
+var pr_encontrados = [];
+var identificadores_encontrados = [];
+var operadores_encontrados = [];
 var constantes_encontrados = [];        // Somente números inteiros
-var terminadores_encontrados = [];      
+var terminadores_encontrados = [];
 var aspas_abertas = false;              // Se for true todo o conteúdo deve ser salvo como um só
 var rastro = "nada";                    // Identifica o tipo de caractere anterior ao atual (identificar sequencias)
 var cont_linha = 0;                     // Identifica a linha de um token
 var cont_coluna = 0;
 
 
-for(caractere of txt_entrada){
-console.log(`cont coluna ${cont_coluna}`);
-    if(aspas_abertas){
-        if(caractere == '"'){
-            aspas_abertas = !aspas_abertas;
-            rastro = "constante";
-            token_atual = checar_token(token_atual);
-        }
-        token_atual += caractere;
-        rastro = "terminador";
+for (caractere of txt_entrada) {
+  console.log(`cont coluna ${cont_coluna}`);
+  if (aspas_abertas) {
+    if (caractere == '"') {
+      aspas_abertas = !aspas_abertas;
+      rastro = "constante";
+      token_atual = checar_token(token_atual);
     }
+    token_atual += caractere;
+    rastro = "terminador";
+  }
 
-    else if(caractere == "\n"){
-        cont_linha++;
-        cont_coluna = 0;
-    }
-    
-    else{
-        if(!eh_espaco(caractere)){
-            if(!eh_operador(caractere)){
-                if(!eh_terminador(caractere)){
-                    if(!eh_numero(caractere)){
-                        if(rastro != "letra"){
-                            token_atual = checar_token(token_atual);
-                        }
-                        token_atual += caractere;
-                        rastro = "letra";
-                    }
-                    else if(Number.isInteger(parseInt(token_atual[0]))){
-                        token_atual += caractere;
-                        rastro = "constante";
-                    }
-                    else{
-                        token_atual = checar_token(token_atual);
-                        token_atual += caractere;
-                        rastro = "constante";
-                    }
-                }
-                else{
-                    token_atual = checar_token(token_atual);
-                    token_atual += caractere;
-                    rastro = "terminador";
-                    if(caractere == '"'){
-                        aspas_abertas = !aspas_abertas;
-                        token_atual = checar_token(token_atual);
-                    }
-                }
+  else if (caractere == "\n") {
+    cont_linha++;
+    cont_coluna = 0;
+  }
+
+  else {
+    if (!eh_espaco(caractere)) {
+      if (!eh_operador(caractere)) {
+        if (!eh_terminador(caractere)) {
+          if (!eh_numero(caractere)) {
+            if (rastro != "letra") {
+              token_atual = checar_token(token_atual);
             }
-            else if(operadores.includes(token_atual[0])){
-                token_atual += caractere;
-                rastro = "operador";
-            }
-            else{
-                token_atual = checar_token(token_atual);
-                token_atual += caractere;
-                rastro = "operador";
-            }
-        }
-        else{
+            token_atual += caractere;
+            rastro = "letra";
+          }
+          else if (Number.isInteger(parseInt(token_atual[0]))) {
+            token_atual += caractere;
+            rastro = "constante";
+          }
+          else {
             token_atual = checar_token(token_atual);
-            rastro = "nada";
+            token_atual += caractere;
+            rastro = "constante";
+          }
         }
+        else {
+          token_atual = checar_token(token_atual);
+          token_atual += caractere;
+          rastro = "terminador";
+          if (caractere == '"') {
+            aspas_abertas = !aspas_abertas;
+            token_atual = checar_token(token_atual);
+          }
+        }
+      }
+      else if (operadores.includes(token_atual[0])) {
+        token_atual += caractere;
+        rastro = "operador";
+      }
+      else {
+        token_atual = checar_token(token_atual);
+        token_atual += caractere;
+        rastro = "operador";
+      }
     }
-    cont_coluna++;
+    else {
+      token_atual = checar_token(token_atual);
+      rastro = "nada";
+    }
+  }
+  cont_coluna++;
 }
 
 console.log(`palavras reservadas: ${pr_encontrados}
@@ -99,46 +99,46 @@ console.log(`palavras reservadas: ${pr_encontrados}
 \nconstantes: ${constantes_encontrados}
 \ncont linha ${cont_linha}`);
 
-function checar_token(token_atual){
-    if(token_atual != ''){
-        switch(rastro) {
-            case "nada":
-                break;
-            case "letra":
-                if(palavras_reservadas.includes(token_atual)){
-                    pr_encontrados.push(token_atual);
-                }
-                else if(token_atual != "\n"){
-                    identificadores_encontrados.push(token_atual);
-                }
-                break;
-            case "operador":
-                operadores_encontrados.push(token_atual);
-                break;
-            case "constante":
-                constantes_encontrados.push(token_atual);
-                break;
-            case "terminador":
-                terminadores_encontrados.push(token_atual);
-                break;
+function checar_token(token_atual) {
+  if (token_atual != '') {
+    switch (rastro) {
+      case "nada":
+        break;
+      case "letra":
+        if (palavras_reservadas.includes(token_atual)) {
+          pr_encontrados.push(token_atual);
         }
+        else if (token_atual != "\n") {
+          identificadores_encontrados.push(token_atual);
+        }
+        break;
+      case "operador":
+        operadores_encontrados.push(token_atual);
+        break;
+      case "constante":
+        constantes_encontrados.push(token_atual);
+        break;
+      case "terminador":
+        terminadores_encontrados.push(token_atual);
+        break;
     }
-    return '';
+  }
+  return '';
 }
 
-function eh_espaco(caractere){
-    return caractere === ' ';
+function eh_espaco(caractere) {
+  return caractere === ' ';
 }
 
-function eh_operador(caractere){
-    return operadores.includes(caractere);
+function eh_operador(caractere) {
+  return operadores.includes(caractere);
 }
 
-function eh_terminador(caractere){
-    return terminadores.includes(caractere)
+function eh_terminador(caractere) {
+  return terminadores.includes(caractere)
 }
 
-function eh_numero(caractere){
-    var n = parseInt(caractere);
-    return Number.isInteger(n);
+function eh_numero(caractere) {
+  var n = parseInt(caractere);
+  return Number.isInteger(n);
 }
